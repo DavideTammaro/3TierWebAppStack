43d3fa68f80957f92bf7774e718c767a2481276e
Davide Tammaro <39389581+DavideTammaro@users.noreply.github.com>
2018-11-22 18:45:58 +0100

    Create webserver1.sh

---
+++ webserver1.sh
@@ -0,0 +1,8 @@
+#webserver1.sh
+pkill tmux
+iptables -F
+echo y | yum install tmux
+echo y | yum install socat
+#tmux new-session -s listner -d "socat - TCP-LISTEN:443,fork"
+tmux new-session -s connect-to-app01 -d "/usr/bin/nc appserver1 8080" #####change the port if the listner in appserver1 has changed####
+tmux new-session -s send-data-to-target-appserver1 -d "/usr/bin/watch /usr/bin/tmux send-keys -t connect-to-app01 data-from-web01 ^M"
