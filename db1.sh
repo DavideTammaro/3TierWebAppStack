54c584ed751f33476c4b221e4d9857d8ac0a8e14
Davide Tammaro <39389581+DavideTammaro@users.noreply.github.com>
2018-11-22 18:51:51 +0100

    Update db1.sh

--- db1.sh
+++ db1.sh
@@ -1,7 +1,7 @@
 #db1
-#if you need to change the listner port, remember to change also the nc port on appserver1.sh script
+#if you need to change the listener port, remember to change also the nc port on appserver1.sh script
 iptables -F
 pkill tmux
 echo y | yum install tmux
 echo y | yum install socat
-tmux new-session -s listner -d "socat - TCP-LISTEN:1433,fork"
+tmux new-session -s listener -d "socat - TCP-LISTEN:1433,fork"
